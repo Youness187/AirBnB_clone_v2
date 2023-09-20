a35ee653e25d14dcbc32ccc1a6e28d2b53533c12
younesse el-mouden <dayouness187@gmail.com>
2023-09-20 05:04:33 +0000

    8. DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -14,3 +14,4 @@ class User(BaseModel, Base):
     password = Column("password", String(128), nullable=False)
     first_name = Column("first_name", String(128), nullable=True)
     last_name = Column("last_name", String(128), nullable=True)
+    places = relationship("Place", backref="user")
