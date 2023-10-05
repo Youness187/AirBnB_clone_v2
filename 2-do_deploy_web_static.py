eccad3dc0c2592ec16601543ad9ccdeb12b47be7
younesse el-mouden <dayouness187@gmail.com>
2023-10-05 21:05:02 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,34 +2,56 @@
 """
 # Fabric script that distributes an archive to your web servers
 """
-from fabric.api import run, env, put
-from os import path
+from fabric.api import run, env, put, task, local
+from datetime import datetime
+import os
 
 env.hosts = ["54.173.75.28", "54.197.74.184"]
 env.user = "ubuntu"
 
 
+def do_pack():
+    """
+    Fabric script that generates a .tgz
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    date_format = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(date_format)
+
+    if local("tar -cvzf versions/{} web_static".format(file_name)).succeeded:
+        return "versions/{}".format(file_name)
+    return None
+
+
+@task
 def do_deploy(archive_path):
     """
     Fabric script that distributes an archive to your web servers
     """
-    if not path.exists(archive_path):
+    try:
+        if not os.path.exists(archive_path):
+            return False
+
+        archive_file = os.path.basename(archive_path)
+        release_path = "/data/web_static/releases/{}".format(
+            os.path.splitext(archive_file)[0]
+        )
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(release_path))
+
+        run("tar -xzf /tmp/{} -C {}".format(archive_file, release_path))
+
+        run("rm /tmp/{}".format(archive_file))
+
+        run("mv {}/web_static/* {}/".format(release_path, release_path))
+
+        run("rm -rf {}/web_static".format(release_path))
+
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s {} /data/web_static/current".format(release_path))
+
+        return True
+    except Exception:
         return False
-    put(archive_path, "/tmp/")
-    name = archive_path.split("/")[-1][:-4]
-    run("mkdir -p /data/web_static/releases/{}".format(name))
-    run(
-        "tar -xzf /tmp/{}.tgz -C ".format(name)
-        + "/data/web_static/releases/{}/".format(name)
-    )
-    run("rm /tmp/{}.tgz".format(name))
-    run(
-        "mv /data/web_static/releases/{}/web_static/* ".format(name)
-        + "/data/web_static/releases/{}/".format(name)
-    )
-    run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-    run("rm -rf /data/web_static/current")
-    run(
-        "ln -s /data/web_static/releases/{}/ ".format(name) + "/data/web_static/current"
-    )
-    return True
