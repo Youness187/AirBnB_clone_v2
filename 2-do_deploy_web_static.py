16eb92714ac62e01d0aae204aef2e7b3323c1a9a
younesse el-mouden <dayouness187@gmail.com>
2023-10-07 11:43:33 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,30 +2,13 @@
 """
 # Fabric script that distributes an archive to your web servers
 """
-from fabric.api import run, env, put, task
-from datetime import datetime
+from fabric.api import run, env, put
 import os
 
 env.hosts = ["54.173.75.28", "54.197.74.184"]
 env.user = "ubuntu"
 
 
-@task
-def do_pack():
-    """
-    Fabric script that generates a .tgz
-    """
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    date_format = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "web_static_{}.tgz".format(date_format)
-
-    if local("tar -cvzf versions/{} web_static".format(file_name)).succeeded:
-        return "versions/{}".format(file_name)
-    return None
-
-
-@task
 def do_deploy(archive_path):
     """
     Fabric script that distributes an archive to your web servers
@@ -49,7 +32,9 @@ def do_deploy(archive_path):
 
         run("rm -rf {}/web_static".format(release_path))
 
-        run("rm -rf /data/web_static/current")
+        old_symlink = "/data/web_static/current"
+        if os.path.exists(old_symlink):
+            run("rm {}".format(old_symlink))
 
         run("ln -s {} /data/web_static/current".format(release_path))
 
