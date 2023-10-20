2d164967a83242ce026e6fba48513f2f78e85c5f
younesse el-mouden <dayouness187@gmail.com>
2023-10-20 21:22:55 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello(strict_slashes=False):
+    """Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb(strict_slashes=False):
+    """HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
