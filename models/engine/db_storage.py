7ee321cc6cc780a4c2060c596aab02c09bd24535
younesse el-mouden <dayouness187@gmail.com>
2023-10-21 21:02:11 +0000

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,7 +76,8 @@ class DBStorage:
     def reload(self, remove=False):
         """reload"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         if remove:
             Session.remove()
