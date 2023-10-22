44dd3782c88e84d191515056ea8d893a49a47b2f
younesse el-mouden <dayouness187@gmail.com>
2023-10-22 01:39:24 +0000

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """States and state"""
-
-
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
